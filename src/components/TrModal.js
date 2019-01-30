40d323dc88bd75ccdc6f52a658d7ff7ad839eb3c
deanban <pbp2005@gmail.com>
2019-01-29 21:01:39 -0500

    reverted

--- src/components/TrModal.js
+++ src/components/TrModal.js
@@ -25,8 +25,10 @@ export default class TrModal extends PureComponent {
   renderModalInfo = ({ placeInfo }) => {
     if (placeInfo.length === 1) {
       return placeInfo.map(place => (
-        <div key={place.referralId}>
-          <h5>{place.venue.location.formattedAddress[0]}</h5>
+        <div>
+          <h5 key={place.referralId}>
+            {place.venue.location.formattedAddress[0]}
+          </h5>
           <h6>{place.venue.location.formattedAddress[1]}</h6>
           <h6>{place.venue.location.formattedAddress[2]}</h6>
           <hr />
@@ -75,6 +77,8 @@ export default class TrModal extends PureComponent {
   );
 
   render() {
+    // this.getInfo();
+    console.log(this.state);
     return ReactDOM.createPortal(this.renderModal(this.props), modalRoot);
   }
 }
